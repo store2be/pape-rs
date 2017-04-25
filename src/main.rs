a89c16270b1cd0976c74e49a51a35b1583b7540d
Tom Houlé <tom@tomhoule.com>
2017-04-25 21:22:15 +0200

    🔨 Use lib.rs from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,6 @@
-#[macro_use]
-extern crate error_chain;
-extern crate futures;
-extern crate hyper;
-#[macro_use]
-extern crate log;
-extern crate mktemp;
-extern crate tera;
-extern crate tokio_service;
-extern crate serde;
-extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
-extern crate slog;
 extern crate slog_envlogger;
-extern crate tokio_core;
-extern crate tokio_io;
-extern crate tokio_process;
 
-mod server;
-mod error;
-mod http_client;
-mod papers;
-mod workspace;
-
+extern crate papers;
 
 fn main() {
     let _logger = slog_envlogger::init().unwrap();
